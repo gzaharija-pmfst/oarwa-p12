8750c73f663079d7b1d27606b1da34c6701222e7
gzaharija <gzaharija@pmfst.hr>
2020-11-10 01:49:34 +0100

    pocetni izgled

--- cypress/integration/poruke_app.spec.js
+++
@@ -1,7 +0,0 @@
-describe('Poruke aplikacija', () => {
-  it('naslovna stranica se otvara', () => {
-    cy.visit('http://localhost:3000')
-    cy.contains('Poruke - Novo')
-    cy.contains('Alati i okviri za razvoj web aplikacija!')
-  })
-})
--- src/App.js
+++ src/App.js
@@ -1,141 +1,63 @@
-import React, { useState, useEffect, useRef } from 'react'
-import Poruka from './components/Poruka'
-import Footer from './components/Footer'
-import LoginForma from './components/LoginForma'
-import porukeServer from './services/poruke'
-import prijavaMetode from './services/login'
-import Promjenjiv from './components/Promjenjiv'
-import PorukaForma from './components/PorukaForma'
+import React, { useState } from 'react'
 
+const Pocetna = () => (
+  <div>
+    <h2>OARWA - Poruke app</h2>
+    <p> Opis aplikacije</p>
+  </div>
+)
 
-const App = (props) => {
-  const [poruke, postaviPoruke] = useState([])
-  const [ispisiSve, postaviIspis] = useState(true)
-  const [username, postaviUsername] = useState('')
-  const [pass, postaviPass] = useState('')
-  const [korisnik, postaviKorisnika] = useState(null)
+const Poruke = () => (
+  <div>
+    <h2>Poruke</h2>
+    <p>Popis poruka</p>
+  </div>
+)
 
-  const porukaFormaRef = useRef()
+const Korisnici = () => (
+  <div>
+    <h2>Korisnici</h2>
+    <p>Popis korisnika</p>
+  </div>
+)
 
-  useEffect(() => {
-    console.log("Effect hook");
-    porukeServer
-      .dohvatiSve()
-      .then(pocPoruke => {
-        console.log("Podaci su učitani");
-        postaviPoruke(pocPoruke)
-      })
-  }, [])
+const App = () => {
+  const [stranica, postaviStranicu] = useState('pocetna')
 
-  useEffect(() => {
-    const logiraniKorisnikJSON = window.localStorage.getItem('prijavljeniKorisnik')
-    if (logiraniKorisnikJSON) {
-      const korisnik = JSON.parse(logiraniKorisnikJSON)
-      postaviKorisnika(korisnik)
-      porukeServer.postaviToken(korisnik.token)
-    }
-  }, [])
-
-  console.log("Renderirano je", poruke.length, "objekata")
-
-  const porukeZaIspis = ispisiSve ? poruke : poruke.filter(p => p.vazno === true)
-
-  const novaPoruka = (noviObjekt) => { 
-    porukaFormaRef.current.promjenaVidljivosti()
-    porukeServer
-      .stvori(noviObjekt)
-      .then((response) => {
-        console.log(response)
-        postaviPoruke(poruke.concat(response))
-      })
+  const naStranicu = (stranica) => (event) => {
+    event.preventDefault()
+    postaviStranicu(stranica)
   }
 
-  const promjenaVaznostiPoruke = (id) => {
-    const poruka = poruke.find(p => p.id === id)
-    const novaPoruka = {
-      ...poruka,
-      vazno: !poruka.vazno
-    }
-    porukeServer
-      .osvjezi(id, novaPoruka)
-      .then((response) => {
-        console.log(response);
-        postaviPoruke(poruke.map(p => p.id !== id ? p : response.data))
-      })
-  }
-  const brisiPoruku = (id) => {
-    porukeServer
-      .brisi(id)
-      .then(response => {
-        console.log(response);
-        postaviPoruke(poruke.filter(p => p.id !== id))
-      })
-  }
-  const userLogin = async (e) => {
-    e.preventDefault()
-    try {
-      const korisnik = await prijavaMetode.prijava({
-        username, pass
-      })
-      window.localStorage.setItem('prijavljeniKorisnik', JSON.stringify(korisnik))
-      porukeServer.postaviToken(korisnik.token)
-      postaviKorisnika(korisnik)
-      postaviUsername('')
-      postaviPass('')
-    } catch (exception) {
-      console.log(exception);
-      alert('Neispravni podaci')
+  const sadrzaj = () => {
+    if (stranica === 'pocetna') {
+      return <Pocetna />
+    } else if (stranica === 'poruke') {
+      return <Poruke />
+    } else if (stranica === 'korisnici') {
+      return <Korisnici />
     }
   }
 
-
-  const porukaForma = () => (
-    <Promjenjiv natpis='Nova poruka' ref={porukaFormaRef}>
-      <PorukaForma
-        spremiPoruku={novaPoruka}
-      />
-    </Promjenjiv>
-  )
-
-  const loginForma = () => {
-    return (
-      <Promjenjiv natpis='Prijavi se'>
-        <LoginForma
-          username={username}
-          pass={pass}
-          promjenaImena={({ target }) => postaviUsername(target.value)}
-          promjenaLozinke={({ target }) => postaviPass(target.value)}
-          userLogin={userLogin}
-        />
-      </Promjenjiv>
-    )
+  const padding = {
+    padding: 5
   }
 
   return (
-    <div>
-      <h1>Poruke - Novo</h1>
-      {korisnik === null
-        ? loginForma()
-        : <div>
-          <p>Prijavljeni ste kao: {korisnik.ime}</p>
-          {porukaForma()}
-        </div>
-      }
+    <div style={padding} >
       <div>
-        <button onClick={() => postaviIspis(!ispisiSve)}>
-          Prikaži {ispisiSve ? "samo važne" : "sve"}
-        </button>
+        <a href="" onClick={naStranicu('pocetna')} style={padding} >
+          pocetna
+        </a>
+        <a href="" onClick={naStranicu('poruke')} style={padding}>
+          poruke
+        </a>
+        <a href="" onClick={naStranicu('korisnici')} style={padding}>
+          korisnici
+        </a>
       </div>
-      <ul>
-        {porukeZaIspis.map(p =>
-          <Poruka
-            key={p.id}
-            poruka={p}
-            brisiPoruku={() => brisiPoruku(p.id)}
-            promjenaVaznosti={() => promjenaVaznostiPoruke(p.id)} />
-        )}
-      </ul>
-      <Footer />
+
+      {sadrzaj()}
     </div>
   )
 }
