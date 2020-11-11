ee3310bcc67a74b4763d06af5c0f617593a9c689
gzaharija <gzaharija@pmfst.hr>
2020-11-11 01:02:29 +0100

    Pocetna aplikacija

--- src/services/login.js
+++
@@ -1,9 +0,0 @@
-import axios from 'axios'
-const osnovniUrl = '/api/login'
-
-const prijava = async podaci => {
-  const odgovor = await axios.post(osnovniUrl, podaci)
-  return odgovor.data
-}
-
-export default {prijava}
